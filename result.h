5644b549f2b4c39b30b2a795e053290edc3fe254
Jean-Marc Lefébure <lefebure.jeanmarc@gmail.com>
2011-06-17 11:40:05 +0300

    generic tree structure

---
+++ result.h
@@ -0,0 +1,27 @@
+#ifndef RESULT_H
+#define RESULT_H
+#include <vector>
+#include <iostream>
+using namespace std;
+//name conflict with another `result'
+template<typename T> struct _result_{
+	T t;
+	vector<_result_> v;
+	_result_(T t):t(t){}
+	_result_(){}
+	void add(T _t,int depth){
+		if(depth==0)
+			//v.push_back(result(_t));	
+			v.insert(v.begin(),_result_(_t));	
+		else	
+			//v.back().add(_t,depth-1);
+			v.front().add(_t,depth-1);
+	}
+};
+//default, to be specialized
+template<typename T> ostream& operator<<(ostream& os,_result_<T>& r){
+	os<<"("<<r.t;
+	for(typename vector<_result_<T> >::iterator i=r.v.begin();i<r.v.end();++i) os<<*i;
+	return os<<")";
+}
+#endif
