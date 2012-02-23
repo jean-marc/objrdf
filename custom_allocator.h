34d52691ba623295f3825ab7c988c56ec3f735bb
Jean-Marc Lefébure <lefebure.jeanmarc@gmail.com>
2012-02-23 10:03:39 +0300

    reuse pseudo_ptr to create vector allocator
    simple allocator, generates a forward linked list of empty cell clusters.
    When allocating each link is visited until a big enough cluster is found
    or end of list is reached, in the latter case 0 is returned and allocation
    has failed.
    Improvement: order linked list, use external array for fast retrieval

--- custom_allocator.h
+++ custom_allocator.h
@@ -2,77 +2,18 @@
 #define CUSTOM_ALLOCATOR_H
 /*
  *	create a custom vector allocator 
- *
+ *	works only for big objects (so we can reuse cells to store information)
+ *	or small objects but not too many of them
  */
-#include <vector>
-#include <iterator>
-using namespace std;
-enum{N=1024};
-template<typename T> struct pseudo_ptr{
-	typedef char INDEX;
-	struct pool{
-		T* v;
-		INDEX index;
-		pool():index(0),v((T*)new char[N*sizeof(T)]){}
-		static pool& get_instance(){
-			static pool* p=new pool();
-			return *p;
-		}
-		/*
- 		*	very dumb, will run out of memory
- 		*/ 
-		pseudo_ptr allocate(size_t num){
-			pseudo_ptr p(index);
-			index+=num;
-			return p;
-		}
-		void deallocate(pseudo_ptr p){}
-	};
-	struct pseudo_ref{
-		/*const*/ INDEX index;
-		explicit pseudo_ref(INDEX index):index(index){}
-		pseudo_ptr operator&(){return pseudo_ptr(index);}
-		operator T&() const{return *(pool::get_instance().v+index);}
-		/*const*/ pseudo_ref& operator=(const T& t){
-			pool::get_instance().v[index]=t;
-			return *this;
-		}
-	};
-	typedef random_access_iterator_tag iterator_category;
-	typedef T value_type;
-	typedef size_t difference_type;
-	typedef pseudo_ptr pointer;
-	typedef pseudo_ref reference;
-	INDEX index;
-	/*explicit*/ pseudo_ptr(INDEX index):index(index){}
-	reference operator*()const{return reference(index);}
-	T* operator->()const{return pool::get_instance().v+index;}
-	pseudo_ptr& operator+=(const size_t s){index+=s;return *this;}
-	pseudo_ptr& operator-=(const size_t s){index-=s;return *this;}
-	pseudo_ptr& operator++(){++index;return *this;}
-	pseudo_ptr& operator--(){--index;return *this;}
-	bool operator!=(const pseudo_ptr& p)const{return index!=p.index;}
-	bool operator<(const pseudo_ptr& p)const{return index<p.index;}
-	bool operator==(const pseudo_ptr& p)const{return index==p.index;}
-	operator bool() const{return index;}
-};
-template<typename T> pseudo_ptr<T> operator+(const pseudo_ptr<T>& a,size_t s){
-	pseudo_ptr<T> tmp=a;
-	return tmp+=s;
-}
-template<typename T> pseudo_ptr<T> operator-(const pseudo_ptr<T>& a,size_t s){
-	pseudo_ptr<T> tmp=a;
-	return tmp-=s;
-}
-template<typename T> ptrdiff_t operator-(const pseudo_ptr<T>& a,const pseudo_ptr<T>& b){
-	return a.index-b.index;
-}
+#include "pseudo_ptr.h"
 
+#include <vector>
 template<typename T> struct custom_allocator{
 	typedef T value_type;
 	typedef pseudo_ptr<T> pointer;
 	typedef pointer const_pointer;
-	typedef typename pseudo_ptr<T>::pseudo_ref reference;
+	//typedef typename pseudo_ptr<T>::pseudo_ref reference;
+	typedef typename pseudo_ptr<T>::reference reference;
 	typedef reference const_reference;
 	typedef std::size_t    size_type;
 	typedef std::ptrdiff_t difference_type;
@@ -84,22 +25,20 @@ template<typename T> struct custom_allocator{
 	}
 	*/
 	template<class U> struct rebind{typedef custom_allocator<U> other;};
-	size_type max_size() const throw() {return N;}
+	size_type max_size() const throw() {return pointer::get_pool()->p.n;}
 	pointer allocate (size_type num, const void* = 0) {
 		std::cerr << "allocate " << num << " element(s)" << " of size " << sizeof(T) << std::endl;
-		pointer ret = pointer::pool::get_instance().allocate(num);
+		pointer ret(pointer::get_pool()->template allocate_t<T>(num));
 		std::cerr << " allocated at: " << ret.operator->() << std::endl;
 		return ret;
 	}
-	void construct(pointer p,T value){
-		new(p.operator->())T(value);
-	}
-	void destroy (pointer p) {
-		p->~T();
-	}
+	void construct(pointer p,T value){new(p)T(value);}
+	void construct(T* p,T value){new(p)T(value);}
+	void destroy (pointer p) {p->~T();}
+	void destroy(T* p){p->~T();}
 	void deallocate (pointer p, size_type num) {
 		std::cerr << "deallocate " << num << " element(s)" << " of size " << sizeof(T) << " at: " << p.operator->() << std::endl;
-		pointer::pool::get_instance().deallocate(p);
+		pointer::get_pool()->template deallocate_t<T>(p.index,num);
 	}
 };
 #endif
