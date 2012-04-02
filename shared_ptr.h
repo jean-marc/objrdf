8377a229033a252078395cbf0d44bc90267fbdaf
Jean-Marc Lefébure <lefebure.jeanmarc@gmail.com>
2012-04-02 20:27:22 +0300

    support for persistence

--- shared_ptr.h
+++ shared_ptr.h
@@ -15,25 +15,67 @@
 #include <iostream>
 #endif
 namespace special{
-	template<typename T> class shared_ptr{
-	private:
-		T* t;
+	template<typename T> struct deleter;
+}
+#ifdef PERSISTENT
+#include "pseudo_ptr.h"
+
+//needed by shared_ptr
+namespace special{
+	template<typename T,typename STORE,bool POLYMORPHISM> struct deleter<pseudo_ptr<T,STORE,POLYMORPHISM>>{
+		static void go(pseudo_ptr<T,STORE,POLYMORPHISM> p){
+			p.destroy();
+			//p->~T();//that's assuming the destructor is virtual
+			//p.pool_index->deallocate(p);
+		}
+	};
+}
+#endif
+namespace special{
+	template<typename T> struct storage{
+		static void* get(size_t s){return ::operator new(s);}
+		static void release(void* p){::operator delete(p);}
+	};
+	/*
+ 	*	can we make it work with pseudo_ptr?
+ 	*/
+	template<typename T> struct deleter<T*>{
+		static void go(T* t){delete t;}
+	};
+	template<
+		typename T,
+		#ifdef PERSISTENT
+		//typename _pointer_=pseudo_ptr<T,free_store<256>,true>,
+		typename _pointer_=pseudo_ptr<T,persistent_store,true>,
+		//typename _pointer_=pseudo_ptr<T,free_store,true>,
+		#else
+		typename _pointer_=T*,
+		#endif
+		typename _reference_=T&
+	> class shared_ptr{
 	public:
 		typedef T element_type;
+		typedef _pointer_ pointer;
+		typedef _reference_ reference;
+	private:
+		pointer t;
+	public:
 		shared_ptr():t(0){}
-		explicit shared_ptr(T* t):t(t){
+		explicit shared_ptr(pointer t):t(t){
 			//std::cerr<<"shared_ptr "<<this<<"\t"<<t<<std::endl;
 			if(t) ++t->n;
 		}
-		inline T* operator->() const{return t;}
-		inline T* get() const{return t;}
-		inline T& operator*() const{return *t;}
+		inline pointer operator->() const{return t;}
+		inline pointer get() const{return t;}
+		inline reference operator*() const{return *t;}
 		int use_count() const{return t ? t->n : 0;}
 		shared_ptr(const shared_ptr& s):t(s.t){
 			//std::cerr<<"shared_ptr "<<this<<"\t"<<t<<std::endl;
 			if(t) ++(t->n);
 		}
 		template<typename S> shared_ptr(const shared_ptr<S>& s):t(s.get()){
+			s.get().print(std::cerr);
+			t.print(std::cerr);
 			if(t) ++(t->n);
 		}
 		shared_ptr& operator=(const shared_ptr& s){
@@ -42,11 +84,12 @@ namespace special{
  			*/	
 			if(t){
 				#ifdef OBJRDF_VERB
-				std::cerr<<t<<"\tattempting to delete `"<<t->id<<"'\t"<<t->n<<std::endl;
+				std::cerr<<(void*)t<<"\tattempting to delete `"<<t->id<<"'\t"<<t->n<<std::endl;
 				#endif
 				--(t->n);
 				if(t->n==0){
-					delete t;
+					//delete t;
+					deleter<pointer>::go(t);
 					#ifdef OBJRDF_VERB
 					std::cerr<<"deleted"<<std::endl;
 					#endif
@@ -63,11 +106,16 @@ namespace special{
 		~shared_ptr(){
 			if(t){
 				#ifdef OBJRDF_VERB
-				std::cerr<<t<<"\tattempting to delete `"<<t->id<<"'\t"<<t->n<<std::endl;
+				t.print(std::cerr);
+				std::cerr<<(void*)t<<"\tattempting to delete `"<<t->id<<"'\t"<<t->n<<std::endl;
 				#endif
 				--(t->n);
+				//there should be cycle detection otherwise infinite loop
 				if(t->n==0){
-					delete t;
+					//what if not allocated on the free store
+					//std::shared_ptr uses a `deleter' class 
+					//delete t;
+					deleter<pointer>::go(t);
 					#ifdef OBJRDF_VERB
 					std::cerr<<"deleted from "<<this<<std::endl;
 					#endif
@@ -75,8 +123,9 @@ namespace special{
 			}
 		}
 	};
+	//needs to be specialized
 	template<typename S,typename T> shared_ptr<S> static_pointer_cast(const shared_ptr<T>& s){
-		return shared_ptr<S>(static_cast<S*>(s.get()));
+		return shared_ptr<S>(static_cast<typename shared_ptr<S>::pointer>(s.get()));
 	} 
 }
 #endif
