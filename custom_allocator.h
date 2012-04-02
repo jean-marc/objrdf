d178ca73d5448772469c134fc1b7b41a1cf9946f
Jean-Marc Lefébure <lefebure.jeanmarc@gmail.com>
2012-04-02 20:26:39 +0300

    made more generic

--- custom_allocator.h
+++ custom_allocator.h
@@ -8,12 +8,16 @@
 #include "pseudo_ptr.h"
 
 #include <vector>
-template<typename T> struct custom_allocator{
+template<
+	typename T,
+	typename _pointer_//=pseudo_ptr<T>
+> struct custom_allocator{
 	typedef T value_type;
-	typedef pseudo_ptr<T> pointer;
+	typedef _pointer_ pointer;
 	typedef pointer const_pointer;
 	//typedef typename pseudo_ptr<T>::pseudo_ref reference;
-	typedef typename pseudo_ptr<T>::reference reference;
+	//typedef typename pseudo_ptr<T>::reference reference;
+	typedef typename pointer::reference reference;
 	typedef reference const_reference;
 	typedef std::size_t    size_type;
 	typedef std::ptrdiff_t difference_type;
@@ -24,7 +28,9 @@ template<typename T> struct custom_allocator{
 	   return &value;
 	}
 	*/
-	template<class U> struct rebind{typedef custom_allocator<U> other;};
+	//what is that for?
+	//template<class U> struct rebind{typedef custom_allocator<U> other;};
+	template<class U> struct rebind{typedef custom_allocator other;};
 	size_type max_size() const throw() {return pointer::get_pool()->p.n;}
 	pointer allocate (size_type num, const void* = 0) {
 		std::cerr << "allocate " << num << " element(s)" << " of size " << sizeof(T) << std::endl;
@@ -32,9 +38,9 @@ template<typename T> struct custom_allocator{
 		std::cerr << " allocated at: " << ret.operator->() << std::endl;
 		return ret;
 	}
-	void construct(pointer p,T value){new(p)T(value);}
+	//void construct(pointer p,T value){new(p)T(value);}
 	void construct(T* p,T value){new(p)T(value);}
-	void destroy (pointer p) {p->~T();}
+	//void destroy (pointer p) {p->~T();}
 	void destroy(T* p){p->~T();}
 	void deallocate (pointer p, size_type num) {
 		std::cerr << "deallocate " << num << " element(s)" << " of size " << sizeof(T) << " at: " << p.operator->() << std::endl;
