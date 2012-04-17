92e34f4abd48a14a1a51f54efa692e7eb68feb97
Jean-Marc Lefébure <lefebure.jeanmarc@gmail.com>
2012-04-17 19:48:24 +0300

    add support to sparql

--- example.satcom.cpp
+++ example.satcom.cpp
@@ -12,14 +12,21 @@
  *
  */
 #include "objrdf.h"
+#include "sparql_engine.h"
 using namespace objrdf;
 
 namespace sat{
+	typedef persistent_store STORE;
 	RDFS_NAMESPACE("http://www.example.org/satcom#","sat")
+	CLASS0(Polarization);
+	//the nice thing about set pointers is we don't have to store them, actually we should never store them
+	PROPERTY(polarization,pseudo_ptr<const Polarization,free_store,false/*,uint8_t*/>);//we can use a set pointer for convenience
+	CLASS(Beam,std::tuple<polarization>);
+	PROPERTY(beam,pseudo_ptr<Beam,STORE>);
 	PROPERTY(angle,short);//+-180 degree
 	PROPERTY(cf,int);
 	CLASS(Carrier,std::tuple<cf>);
-	PROPERTY(carrier,Carrier*);
+	PROPERTY(carrier,pseudo_ptr<Carrier,STORE>);
 	//CLASS1(Transponder,array<carrier>);
 	//CLASS(Transponder,std::tuple<array<carrier>>);
 	char _Transponder[]="Transponder";
@@ -27,7 +34,8 @@ namespace sat{
 		rdfs_namespace,
 		_Transponder,
 		std::tuple<
-			array<carrier>
+			array<carrier>,
+			beam
 		>,
 		Transponder
 	>{
@@ -48,11 +56,7 @@ namespace sat{
 		}
 
 	};
-	CLASS(Beam,std::tuple<>);
-	PROPERTY(beam,Beam*);
-	CLASS0(Polarization);
-	PROPERTY(polarization,Polarization*);//we can use a set pointer for convenience
-	PROPERTY(transponder,Transponder*);
+	PROPERTY(transponder,pseudo_ptr<Transponder,STORE>);
 	//what about
 	//__PROPERTY(xpr,pseudo_ptr<Transponder,free_store<>>);
 	char _Satellite[]="Satellite";
@@ -87,13 +91,18 @@ namespace sat{
 }
 using namespace sat;
 int main(int argc, char* argv[]){
+	polarization::PTR::construct_at(1,uri("CW"));
+	polarization::PTR::construct_at(2,uri("CCW"));
+	polarization::PTR::construct_at(3,uri("VERTICAL"));
+	polarization::PTR::construct_at(4,uri("HORIZONTAL"));
 	typedef transponder::PTR::pointer T_PTR;
 	typedef carrier::PTR::pointer C_PTR;
-	typedef shared_ptr<Satellite>::pointer S_PTR;	
+	typedef pseudo_ptr<Satellite,STORE> S_PTR;	
 	//needed to load the pools
 	S_PTR::get_pool()->get_size();
 	C_PTR::get_pool()->get_size();
 	T_PTR::get_pool()->get_size();
+	beam::PTR::get_pool();
 	if(argc>1){
 		int n=atoi(argv[1]);
 		for(int i=0;i<n;++i){
@@ -102,5 +111,16 @@ int main(int argc, char* argv[]){
 			S_PTR::construct(Satellite::create(uri(os.str())));
 		}
 	}
-	to_rdf_xml(cout);
+	//to_rdf_xml(cout);
+	//cout<<"Enter SPARQL query:";
+ 	cin.exceptions(iostream::eofbit);
+	sparql_parser sp(std::cin);
+	bool r=sp.go();
+	if(r){
+		cerr<<"parsing success"<<endl;
+		sp.out(cout);
+	}else{
+		cerr<<"parsing failure"<<endl;
+		exit(1);
+	}
 }
