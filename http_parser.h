7ff313f710ff38f2505b743707dbfc1dd363f3ba
Jean-Marc Lefébure <lefebure.jeanmarc@gmail.com>
2013-04-11 23:36:06 +0300

    clean up and direct debug to cerr instead of cout

--- http_parser.h
+++ http_parser.h
@@ -18,21 +18,21 @@ using namespace std;
 //template<typename HANDLER> struct http_parser:char_iterator/*<128>*/{
 struct http_parser:char_iterator/*<128>*/{
 	typedef seq_c<'\r','\n'> crlf;
-	//typedef seq<char_p<'G'>,char_p<'E'>,char_p<'T'> > get;
-	typedef event<plus_p<not_p<char_p<' '> > > > method;//if eof it will loop forever
-	typedef plus_p<not_p<or_p<char_p<' '>,or_p<char_p<'\t'>,or_p<char_p<'\r'>,char_p<'\n'> > > > > > str;//a string
-	typedef event<plus_p<not_p<or_p<char_p<'='>,char_p<' '> > > > > _key;
-	typedef event<plus_p<not_p<or_p<char_p<'&'>,char_p<' '> > > > > _value;
+	//typedef seq<char_p<'G'>,char_p<'E'>,char_p<'T'>> get;
+	typedef event<plus_p<not_p<char_p<' '>>>> method;//if eof it will loop forever
+	typedef plus_p<not_p<or_p<char_p<' '>,or_p<char_p<'\t'>,or_p<char_p<'\r'>,char_p<'\n'>>>>>> str;//a string
+	typedef event<plus_p<not_p<or_p<char_p<'='>,char_p<' '>>>>> _key;
+	typedef event<plus_p<not_p<or_p<char_p<'&'>,char_p<' '>>>>> _value;
 	typedef seq<_key,char_p<'='>,_value> _pair_;
-	struct _pairs_:seq<_pair_,or_p<seq<char_p<'&'>,_pairs_>,true_p> >{}; //recursive
-	typedef	seq<char_p<'?'>,or_p<_pairs_,true_p> > arguments; 
-	typedef event<plus_p<not_p<or_p<char_p<' '>,char_p<'?'> > > > > path;
-	typedef seq<path,or_p<arguments,true_p> > url;
+	struct _pairs_:seq<_pair_,or_p<seq<char_p<'&'>,_pairs_>,true_p>>{}; //recursive
+	typedef	seq<char_p<'?'>,or_p<_pairs_,true_p>> arguments; 
+	typedef event<plus_p<not_p<or_p<char_p<' '>,char_p<'?'>>>>> path;
+	typedef seq<path,or_p<arguments,true_p>> url;
 	typedef str version;
 	typedef seq<method,char_p<' '>,url,char_p<' '>,version,crlf> head;
-	typedef event<plus_p<not_p<or_p<char_p<':'>,char_p<'\r'> > > > > key;
-	typedef event<plus_p<not_p<char_p<'\r'> > > > value;	
-	typedef seq<key,seq_p<char_p<':'>,char_p<' '> >,value,crlf> header;
+	typedef event<plus_p<not_p<or_p<char_p<':'>,char_p<'\r'>>>>> key;
+	typedef event<plus_p<not_p<char_p<'\r'>>>> value;	
+	typedef seq<key,seq_p<char_p<':'>,char_p<' '>>,value,crlf> header;
 	typedef seq<head,kleene_p<header>,crlf> request;
 	http_parser(istream& is):char_iterator/*<128>*/(is){}
 	//bool go(){return request::go(*static_cast<HANDLER*>(this));}
@@ -42,32 +42,32 @@ struct http_parser:char_iterator/*<128>*/{
 	string current_key,current_path,current_method;
 	bool callback(method,string s){
 		current_method=s;
-		cout<<"method:"<<s<<endl;
+		cerr<<"method:"<<s<<endl;
 		return true;
 	}
 	bool callback(key,string s){
 		current_key=s;
-		cout<<"key:"<<s;
+		cerr<<"key:"<<s;
 		return true;
 	}
 	bool callback(value,string s){
 		headers[current_key]=s;
-		cout<<" value:"<<s<<endl;
+		cerr<<" value:"<<s<<endl;
 		return true;
 	}
 	bool callback(path,string s){
 		current_path=s;
-		cout<<"path:"<<s<<endl;
+		cerr<<"path:"<<s<<endl;
 		return true;
 	}
 	bool callback(_key,string s){
 		current_key=s;
-		cout<<"_key:"<<s<<endl;
+		cerr<<"_key:"<<s<<endl;
 		return true;
 	}
 	bool callback(_value,string s){
 		url_arguments[current_key]=s;
-		cout<<"_value:"<<s<<endl;
+		cerr<<"_value:"<<s<<endl;
 		return true;
 	}
 };
@@ -79,27 +79,27 @@ struct generic_http_parser:http_parser<generic_http_parser>{
 	generic_http_parser(istream& is):http_parser<generic_http_parser>(is){}
 	bool callback(method,string s){
 		current_method=s;
-		cout<<"method:"<<s<<endl;
+		cerr<<"method:"<<s<<endl;
 	}
 	bool callback(key,string s){
 		current_key=s;
-		cout<<"key:"<<s<<endl;
+		cerr<<"key:"<<s<<endl;
 	}
 	bool callback(value,string s){
 		headers[current_key]=s;
-		cout<<"value:"<<s<<endl;
+		cerr<<"value:"<<s<<endl;
 	}
 	bool callback(path,string s){
 		current_path=s;
-		cout<<"path:"<<s<<endl;
+		cerr<<"path:"<<s<<endl;
 	}
 	bool callback(_key,string s){
 		current_key=s;
-		cout<<"_key:"<<s<<endl;
+		cerr<<"_key:"<<s<<endl;
 	}
 	bool callback(_value,string s){
 		url_arguments[current_key]=s;
-		cout<<"_value:"<<s<<endl;
+		cerr<<"_value:"<<s<<endl;
 	}
 };
 */
