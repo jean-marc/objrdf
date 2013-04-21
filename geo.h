55a4580b57ed6b264b466f6bf4e4eb123d125a63
Jean-Marc Lefébure <lefebure.jeanmarc@gmail.com>
2013-04-21 09:58:57 +0300

    wgs84_pos coordinates

---
+++ geo.h
@@ -0,0 +1,13 @@
+#ifndef GEO_H
+#define GEO_H
+#include "objrdf.h"
+namespace geo{
+	RDFS_NAMESPACE("http://www.w3.org/2003/01/geo/wgs84_pos#","geo")
+	PROPERTY(lat,float);
+	//problem:conflict with type long
+	//PROPERTY(long,float);
+	char _long[]="long";typedef objrdf::property<rdfs_namespace,_long,float> _long_;
+	CLASS(Point,std::tuple<lat,_long_>);
+	//we could define functions that operate on geo:Point eg. distance (see http://www.w3.org/TR/rdf-sparql-query/#extensionFunctions)
+}
+#endif
