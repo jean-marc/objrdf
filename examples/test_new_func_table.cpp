ecdffbeb54b40b4de5751ec48cc94ae858034921
Jean-Marc Lefébure <lefebure.jeanmarc@gmail.com>
2014-10-24 09:32:46 -0700

    help testing migration

--- examples/test_new_func_table.cpp
+++ examples/test_new_func_table.cpp
@@ -4,22 +4,31 @@
  *
  */
 #include "objrdf.h"
+#include "rdf_xml_parser.h"
 using namespace objrdf;
 RDFS_NAMESPACE("http://test.example.org/#","test")
 typedef property<rdfs_namespace,str<'p','_','0'>,int> p_0;
 typedef property<rdfs_namespace,str<'p','_','1'>,int> p_1;
+typedef property<rdfs_namespace,str<'p','_','2'>,base_resource*> p_2;
+typedef property<rdfs_namespace,str<'p','_','3'>,base_resource*> p_3;
 typedef resource<
 	rdfs_namespace,
 	str<'A'>,
-	std::tuple<p_0,p_1>
+	std::tuple<p_0,p_1,p_2,p_3>
 > A;
 
 int main(){
 	for(auto i:A::v)
 		cout<<i<<endl;
 	A a(uri("jm"));
+	base_resource::do_index(base_resource::nil);
+	A::do_index(&a);	
 	a.get<p_0>().t=1234;
 	a.get<p_1>().t=5678;
 	to_rdf_xml(&a,cout);
+	rdf_xml_parser r(cin);
+	r.go();
+	to_rdf_xml(&a,cout);
+	to_rdf_xml(base_resource::nil,cout);	
 
 }
