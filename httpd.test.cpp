5d6141b30e162be4fcb866864066dd93e7937e23
Jean-Marc Lefébure <lefebure.jeanmarc@gmail.com>
2015-10-02 13:19:06 -0700

    switch to std::thread

--- httpd.test.cpp
+++ httpd.test.cpp
@@ -1,7 +1,9 @@
+#include <thread>
 #include "httpd.h"
 #include "objrdf.h"
 
 int main(){
-	objrdf::httpd h;
-	h.run();
+	objrdf::httpd h(8080);
+	auto t=std::thread(&objrdf::httpd::run,&h);
+	t.join();
 };
