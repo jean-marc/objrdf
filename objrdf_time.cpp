60c8eab483bf0424d03315acfbeaee753ef977c2
Jean-Marc Lefébure <lefebure.jeanmarc@gmail.com>
2015-11-03 11:02:03 -0800

    new objrdf::ts ms time stamp property

---
+++ objrdf_time.cpp
@@ -0,0 +1,12 @@
+#include "objrdf_time.h"
+ostream& objrdf::operator<<(ostream& os,const std::chrono::system_clock::time_point& _t){
+	std::time_t t=std::chrono::system_clock::to_time_t(_t);
+	auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(_t.time_since_epoch());
+	char buffer[80];
+	tm* timeinfo=localtime(&t);
+	strftime (buffer,80,"%FT%T",timeinfo);	
+	char ms_buffer[10];	
+	snprintf(ms_buffer,10,".%03lli",(ms.count()%1000));
+	return os<<buffer<<ms_buffer;
+}
+istream& objrdf::operator>>(istream& is,const std::chrono::system_clock::time_point& t){return is;}//no op
---
+++ objrdf_time.h
@@ -0,0 +1,16 @@
+#ifndef OBJRDF_TIME_H
+#define OBJRDF_TIME_H
+#include <chrono>
+#include <objrdf/objrdf.h>
+
+namespace objrdf{
+	template<> struct selector<std::chrono::system_clock::time_point>{
+		typedef xsd::dateTime ResultT;
+		enum{IS_LITERAL=1};
+	};
+	typedef property<objrdf_rdfs_ns,str<'t','s'>,std::chrono::system_clock::time_point> ts;
+	ostream& operator<<(ostream& os,const std::chrono::system_clock::time_point&);
+	istream& operator>>(istream& is,const std::chrono::system_clock::time_point&);
+}
+
+#endif
