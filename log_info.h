6a48fde045d4fda666da1a63c43e4c29391ec976
Jean-Marc Lefebure <lefebure.jeanmarc@gmail.com>
2016-02-17 08:49:34 -0800

    use systemd notation to indicate priority

--- log_info.h
+++ log_info.h
@@ -3,35 +3,35 @@
 //would be nice to define per namespace
 
 #ifndef LOG_EMERGENCY
-#define LOG_EMERGENCY cerr
+#define LOG_EMERGENCY cerr<<"<0>"
 #endif
 
 #ifndef LOG_ALERT
-#define LOG_ALERT cerr
+#define LOG_ALERT cerr<<"<1>"
 #endif
 
 #ifndef LOG_CRITICAL
-#define LOG_CRITICAL cerr
+#define LOG_CRITICAL cerr<<"<2>"
 #endif
 
 #ifndef LOG_ERROR
-#define LOG_ERROR cerr
+#define LOG_ERROR cerr<<"<3>"
 #endif
 
 #ifndef LOG_WARNING
-#define LOG_WARNING cerr
+#define LOG_WARNING cerr<<"<4>"
 #endif
 
 #ifndef LOG_NOTICE
-#define LOG_NOTICE cerr
+#define LOG_NOTICE cerr<<"<5>"
 #endif
 
 #ifndef LOG_INFO
-#define LOG_INFO if(0) cerr
+#define LOG_INFO if(0) cerr<<"<6>"
 #endif
 
 #ifndef LOG_DEBUG
-#define LOG_DEBUG if(0) cerr
+#define LOG_DEBUG if(0) cerr<<"<7>"
 #endif
 
 #endif
