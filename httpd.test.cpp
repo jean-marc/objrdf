fb71a1f3511faeaddf1ab00a1dba0a3288de1d2f
Jean-Marc Lefébure <lefebure.jeanmarc@gmail.com>
2016-05-05 15:52:22 -0700

    fix missing log information

--- httpd.test.cpp
+++ httpd.test.cpp
@@ -2,7 +2,21 @@
 #include "httpd.h"
 #include "objrdf.h"
 
+namespace rdfs{
+	int verbosity=7;
+	const char _context_[]="rdfs";
+}
+namespace objrdf{
+	int verbosity=7;
+	const char _context_[]="objrdf";
+}
+namespace pool_allocator{
+	int verbosity=7;
+	const char _context_[]="pool";
+}
+
 int main(){
+	objrdf::generate_index();
 	objrdf::httpd h(8080);
 	auto t=std::thread(&objrdf::httpd::run,&h);
 	t.join();
