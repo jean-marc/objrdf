70d842dd1f5280d2e0699c55c50b8f38c4a40ba4
Jean-Marc Lefébure <lefebure.jeanmarc@gmail.com>
2016-08-15 15:42:52 -0700

    introduce altitude property inside geo

--- geo.h
+++ geo.h
@@ -4,12 +4,11 @@
 namespace geo{
 	//in degrees
 	RDFS_NAMESPACE("http://www.w3.org/2003/01/geo/wgs84_pos#","geo")
-	PROPERTY(lat,float);
+	typedef objrdf::property<rdfs_namespace,objrdf::str<'l','a','t'>,float> lat;
 	//problem:conflict with type long
-	//PROPERTY(long,float);
-	struct _long{static char* name(){return "long";}};
-	typedef objrdf::property<rdfs_namespace,_long,float> _long_;
-	CLASS(Point,std::tuple<lat,_long_>);
+	typedef objrdf::property<rdfs_namespace,objrdf::str<'l','o','n','g'>,float> _long_;
+	typedef objrdf::property<rdfs_namespace,objrdf::str<'a','l','t'>,float> alt;
+	CLASS(Point,std::tuple<lat,_long_,alt>);
 	//we could define functions that operate on geo:Point eg. distance (see http://www.w3.org/TR/rdf-sparql-query/#extensionFunctions)
 }
 #endif
